d71adaf19aa8248be3712aa98a4136892e6d6275
Nika <akino7983@gmail.com>
2023-12-27 17:17:13 +0300

    costs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,20 @@
-import CostItem from "./components/CostItem/CostItem";
+import Costs from "./components/Costs/Costs";
 
 function App() {
   const costs = [
-    { date: new Date(2021, 2, 12), 
-      description: "Холодильник", 
-      amount: 999.99
+    { date: new Date(2021, 2, 12), description: "Холодильник", amount: 999.99 },
+    {
+      date: new Date(2021, 11, 23),
+      description: "Микроволновка",
+      amount: 234.24,
     },
-    { date: new Date(2021, 11, 23), 
-      description: "Микроволновка", 
-      amount: 234.24
-    },
-    { date: new Date(2021, 6, 7), 
-      description: "Машина", 
-      amount: 9223.99
-    },
+    { date: new Date(2021, 6, 7), description: "Машина", amount: 9223.99 },
   ];
   return (
     <div>
       <h1>React</h1>
-      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} 
-      />
-      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} 
-      />
-      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} 
+      <Costs
+        costs={costs}
       />
     </div>
   );
