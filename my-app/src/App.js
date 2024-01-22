e0d68239c66550d24ded77abcf44dc6ce839851c
Nika <akino7983@gmail.com>
2024-01-22 17:33:10 +0300

    add cost

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,25 @@
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
+import React, {useState} from "react";
+
+const INITIAL_COSTS = [
+  { id: 'c1', date: new Date(2021, 2, 12), description: "Холодильник", amount: 999.99 },
+  {
+    id: 'c2', date: new Date(2021, 11, 23),
+    description: "Микроволновка",
+    amount: 234.24,
+  },
+  { id: 'c3', date: new Date(2021, 6, 7), description: "Машина", amount: 9223.99 },
+];
 
 const App = () => {
-  const costs = [
-    { id: 'c1', date: new Date(2021, 2, 12), description: "Холодильник", amount: 999.99 },
-    {
-      id: 'c2', date: new Date(2021, 11, 23),
-      description: "Микроволновка",
-      amount: 234.24,
-    },
-    { id: 'c3', date: new Date(2021, 6, 7), description: "Машина", amount: 9223.99 },
-  ];
+  
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
 const addCostHandler = (cost) => {
-  console.log(cost);
-  console.log('App');
-  // costs.push(cost);
-  // console.log(costs);
+  setCosts(prevCosts => {
+    return [cost, ...prevCosts]
+  });
 }
 
   return (
